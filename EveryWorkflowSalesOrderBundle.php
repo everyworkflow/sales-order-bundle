6c38378495332299bbc6581a1a620c103cee1d20
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:01:39 +0000

    Added ?ExtensionInterface

--- EveryWorkflowSalesOrderBundle.php
+++ EveryWorkflowSalesOrderBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\SalesOrderBundle;
 
 use EveryWorkflow\SalesOrderBundle\DependencyInjection\SalesOrderExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowSalesOrderBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new SalesOrderExtension();
     }
